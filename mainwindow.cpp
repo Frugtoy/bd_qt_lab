33bdd7e81db0ba7d198c7f31bb69d3f9d15441e1
Frugtoy <borislesin@yandex.ru>
2020-05-11 12:54:35 +0300

    run man, run

---
+++ mainwindow.cpp
@@ -0,0 +1,36 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QApplication>
+#include<QtSql/QSqlDatabase>
+
+#include<QSqlQueryModel>
+#include<QSqlError>
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    connect(ui->admin,&QPushButton::clicked, this,&MainWindow::click);
+    connect(ui->add,&QPushButton::clicked,this,&MainWindow::dialog);
+    db = QSqlDatabase::addDatabase("QSQLITE");
+    db.setDatabaseName("my_awesome_bd");
+    db.open();
+    ui->tableView->setModel(&query_model);
+    faculty_add_window = new Dialog;
+}
+void MainWindow::click(){
+
+    QSqlQuery query;
+    query.exec(ui->textEdit->toPlainText());
+    query_model.setQuery(query);
+    statusBar()->showMessage(query.lastError().text());
+}
+void MainWindow::dialog(){
+     faculty_add_window->exec();
+}
+MainWindow::~MainWindow()
+{
+    delete ui;
+    delete faculty_add_window;
+}
+
---
+++ mainwindow.h
@@ -0,0 +1,33 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+#include <QSqlQuery>
+#include <QMainWindow>
+#include <QSqlTableModel>
+#include <dialog.h>
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+
+private:
+    Ui::MainWindow *ui;
+    Dialog * faculty_add_window;
+    QSqlDatabase db;
+    QSqlQueryModel query_model;
+    QSqlTableModel *model;
+    void dialog();
+    void setupModel(const QString &tableName , const QStringList &headers);
+    void createUI();
+private slots:
+    void click();
+ };
+
+#endif // MAINWINDOW_H
